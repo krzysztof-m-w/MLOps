f852072c46de1d40737b0ec78feaed5357031b71
krzysztof-m-w <krzysztof.m.weber@gmail.com>
2026-01-05 14:18:46 +0100

    ec2 working

--- src/test_request.py
+++ src/test_request.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://localhost:3000/predict"
+url = "http://13.62.100.118:3000/predict"
 data = {
     "payload": {
         "Year": 2020,
@@ -9,7 +9,7 @@ data = {
         "Location": "Iceland-S",
         "Deaths": 2,
     }
-    }
+}
 
 response = requests.post(url, json=data)  # 'json=' automatically sets Content-Type
 print(response.text)
